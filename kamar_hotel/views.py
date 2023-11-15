0038cc8dd8d2708b66da36d84e3f774a9bab3504
rebekaalma <rebekabarimbing@gmail.com>
2023-11-15 16:41:12 +0700

    Menyelesaikan nomor 9 dan 10

--- kamar_hotel/views.py
+++ kamar_hotel/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_daftar_reservasi(request):
+    return render(request, 'daftar-reservasi-kamar.html')
+
+def update_reservasi(request):
+    return render(request, 'update-status-reservasi.html')
+
+def get_detail_reservasi(request):
+    return render(request, 'detail-reservasi.html')
+
+def reservasi_shuttle(request):
+    return render(request, 'reservasi-shuttle.html')
--- sistel/urls.py
+++ sistel/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('feedback/', include('complaint_review.urls')),
+    path('kamar-hotel/', include('kamar_hotel.urls'))
 ]
