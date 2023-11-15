cfc82aaf77b3f6257cfd762a3b9cc7a80cb2b383
V1ctoriaa <npascal70@gmail.com>
2023-11-15 20:52:52 +0700

    Commit pertama navbar dan login serta logout

--- sistel/urls.py
+++ sistel/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from sistel.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('feedback/', include('complaint_review.urls')),
+    path('login/', login, name='login')
 ]
